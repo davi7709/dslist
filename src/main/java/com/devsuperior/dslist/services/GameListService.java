8a3b7a10a2028c3215585ab4177a90999b68aba2
davi7709 <davialealves@gmail.com>
2025-01-26 17:15:53 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -1,32 +1,27 @@
 package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repository.GameListRepository;
 import com.devsuperior.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
-public class GameService {
+public class GameListService {
 
     @Autowired
-    private GameRepository gameRepository;
+    private GameListRepository gameListRepository;
     @Transactional(readOnly = true)
-    public List<GameMinDTO> findAll(){
-        List<Game> game = gameRepository.findAll();
-        return game.stream().map(x -> new GameMinDTO(x)).toList();
+    public List<GameListDTO> findAll(){
+        List<GameList> game = gameListRepository.findAll();
+        return game.stream().map(x -> new GameListDTO(x)).toList();
     }
-
-    @Transactional(readOnly = true)
-    public GameDTO findById(Long id){
-       Game result = gameRepository.findById(id).get();
-        return new GameDTO(result);
-    }
-
 
 }
